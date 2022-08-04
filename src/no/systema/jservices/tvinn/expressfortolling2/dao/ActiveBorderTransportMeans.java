2278b87d951d799691124f1cd249b9f80e1c6c02
sobekpeleteiro <64634843+sobekpeleteiro@users.noreply.github.com>
2022-08-04 11:53:35 +0200

    Update

--- src/no/systema/jservices/tvinn/expressfortolling2/dao/ActiveBorderTransportMeans.java
+++ src/no/systema/jservices/tvinn/expressfortolling2/dao/ActiveBorderTransportMeans.java
@@ -3,22 +3,24 @@ package no.systema.jservices.tvinn.expressfortolling2.dao;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ActiveBorderTransportMeans {
 	
-	private String identificationNumber = "";
-	private String typeOfIdentification = "";
+	private String identificationNumber;
+	private String typeOfIdentification;
 	
-	private String typeOfMeansOfTransport = "";
-	private String nationalityCode = "";
-	private String modeOfTransportCode = "";
-	private String actualDateAndTimeOfDeparture = "";
-	private String estimatedDateAndTimeOfDeparture = "";
-	private String estimatedDateAndTimeOfArrival = "";
+	private String typeOfMeansOfTransport;
+	private String nationalityCode;
+	private String modeOfTransportCode;
+	private String actualDateAndTimeOfDeparture;
+	private String estimatedDateAndTimeOfDeparture;
+	private String estimatedDateAndTimeOfArrival;
 	
 	private Operator operator;
 	private List<Crew> crew;
